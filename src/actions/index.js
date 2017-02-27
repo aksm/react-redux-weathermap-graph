810c6fc17cf3b2a10ec2eecbb5eb7bc3b10f35f6
Al Min <albertmin@Lilu.local>
2017-02-26 22:02:49 -0500

    try cors header with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
 const API_KEY = '20497d7277a6d8e11da54b4af9fbd7c7';
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
-  const request = axios.get(url);
+  const config = {
+    method: 'get',
+    url: url,
+    crossDomain: true
+  };
+  // const request = axios.get(url);
+  const request = axios(config);
 
   return {
     type: FETCH_WEATHER,
