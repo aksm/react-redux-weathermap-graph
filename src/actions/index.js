41a0dd5c81ee47b8cae0228100c8121272e84df3
Al Min <albertmin@Lilu.local>
2017-02-26 21:33:11 -0500

    add proxy to openweather api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '20497d7277a6d8e11da54b4af9fbd7c7';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
